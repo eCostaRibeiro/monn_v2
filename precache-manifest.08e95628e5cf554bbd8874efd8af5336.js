076f2abfd98a924db904fd3c794a467f84b8b443
eCostaRibeiro <ecostaribeiro@outlook.com>
2023-01-14 00:22:50 -0300

    Updates

---
+++ precache-manifest.08e95628e5cf554bbd8874efd8af5336.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b4b97a5dc61aacdc034",
+    "url": "/monn_v2/static/css/main.b107427f.chunk.css"
+  },
+  {
+    "revision": "0b4b97a5dc61aacdc034",
+    "url": "/monn_v2/static/js/main.0b4b97a5.chunk.js"
+  },
+  {
+    "revision": "ae0b8547bbc7695fa595",
+    "url": "/monn_v2/static/js/1.ae0b8547.chunk.js"
+  },
+  {
+    "revision": "7760722422ad1524ad7c",
+    "url": "/monn_v2/static/js/runtime~main.77607224.js"
+  },
+  {
+    "revision": "4a223fa55e20f0c00a73587a86d5a67f",
+    "url": "/monn_v2/index.html"
+  }
+];
